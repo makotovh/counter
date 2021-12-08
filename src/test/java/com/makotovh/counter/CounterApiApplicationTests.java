4cd8796b6eed69910e07a7a5ea385f9c540fefac
makotovh <makotovh@gmail.com>
2021-12-08 22:22:19 +0100

    Initial commit

---
+++ src/test/java/com/makotovh/counter/CounterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makotovh.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
